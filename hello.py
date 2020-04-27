1e1afb244bd3587818ebaf4a0f636119f98edba0
YI ZHENG <yizheng@YIs-MacBook-Pro.local>
2020-04-26 22:29:31 -0500

    template A buy B

--- hello.py
+++ hello.py
@@ -1,10 +1,62 @@
+import spacy
 import nltk
+from collections import Counter
 from nltk.stem import WordNetLemmatizer
+from spacy import displacy
+from nltk.chunk import conlltags2tree, tree2conlltags
+
 import re
 from nltk.sem.relextract import extract_rels, rtuple
 lemmatizer = WordNetLemmatizer()
 
-
+def filter_spans(spans):
+    # Filter a sequence of spans so they don't contain overlaps
+    # For spaCy 2.1.4+: this function is available as spacy.util.filter_spans()
+    get_sort_key = lambda span: (span.end - span.start, -span.start)
+    sorted_spans = sorted(spans, key=get_sort_key, reverse=True)
+    result = []
+    seen_tokens = set()
+    for span in sorted_spans:
+        # Check for end - 1 here because boundaries are inclusive
+        if span.start not in seen_tokens and span.end - 1 not in seen_tokens:
+            result.append(span)
+        seen_tokens.update(range(span.start, span.end))
+    result = sorted(result, key=lambda span: span.start)
+    return result
+#     return relations
+def extract_currency_relations(doc):
+    spans = list(doc.ents) + list(doc.noun_chunks)
+    spans = filter_spans(spans)
+    with doc.retokenize() as retokenizer:
+        for span in spans:
+            retokenizer.merge(span)
+    wordSet = set()
+    relations = []
+    for word in filter(lambda w: w.pos_=="VERB" or w.pos_=="NOUN", doc):
+        # print(word)
+        similarWords = ["acquire","buy","purchase"]
+        for sigleSimilarWords in similarWords:
+            # print(word)
+            if nlp(word.lemma_).similarity(nlp(sigleSimilarWords))>0.7:
+                wordSet.add(word)
+    print(wordSet)
+    for word in wordSet:
+        # print(word.head)
+        childs = word.children
+        # print(list(childs))
+        print(childs.__class__)
+        dobj = []
+        nsubj = []
+        for possible in word.children:
+            print(possible)
+            if possible.dep_ == "nsubj" and (possible.ent_type_ =="ORG" or possible.pos_=="PRON"):
+                nsubj.append(possible)
+            elif possible.dep_=='dobj' and (possible.ent_type_ =="ORG" or possible.pos_=="PRON"):
+                dobj.append(possible)
+        for i in dobj:
+            for j in nsubj:
+                relations.append((j,word,i))
+    return relations
 def lemmatize(tokens):
     lemmaArr = []
     for token in tokens:
@@ -14,70 +66,30 @@ def lemmatize(tokens):
         lemmaArr.append(temp)
     return lemmaArr;
 
-# file = open("WikipediaArticles/Amazon_com.txt")
-file = open("test.txt")
+file = open("WikipediaArticles/Amazon_com.txt")
+# file = open("WikipediaArticles/IBM.txt")
+# file = open("WikipediaArticles/AppleInc.txt")
+# file = open("test.txt")
 
 fl = file.read()
 sentences = nltk.sent_tokenize(fl) # Split the document into sentences
 tokens = [nltk.word_tokenize(sentence) for sentence in sentences] # Tokenize the sentences into words
 lemmaArr = lemmatize(tokens)  # Lemmatize the words to extract lemmas as features
 pos_tag = [nltk.pos_tag(token) for token in lemmaArr] # Part-of-speech (POS) tag the words to extract POS tag features
-# print(pos_tag[0])
-# word_tag_pairs = nltk.bigrams(pos_tag[0])
-# print(list(word_tag_pairs)[0])
-#
-# grammar = r"""
-#
-#     NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
-#   PP: {<IN><NP>}               # Chunk prepositions followed by NP
-#   VP: {<VB.*><NP|PP|CLAUSE>+$} # Chunk verbs and their arguments
-#   CLAUSE: {<NP><VP>}           # Chunk NP, VP
-#   """
-#
-# cp = nltk.RegexpParser(grammar)
-# # cp.evaluate(pos_tag[0])
-# chuck = cp.parse(pos_tag[0])
-# chuck.draw()
 
-# print(chuck)
-print(nltk.ne_chunk(pos_tag[0]))
-#
-# OF = re.compile(r'.*\bat\b.*')
-# # OF2 = re.compile(r'.*\bof\b.*')
-# rels2 = extract_rels('GPE', 'GPE', nameEntity, corpus='ace', pattern=OF, window=10)
-# rels = extract_rels('ORGANIZATION', 'GPE', nameEntity, corpus='ace', pattern=OF2, window=10)
-# print(rels)
-# print(rels2)
-# for each in pos_tag:
-#     nameEntity = nltk.ne_chunk(each)
-# #     # print(nameEntity)
-# #
-# #     OF = re.compile(r'.*\bat\b.*')
-# #     OF2 = re.compile(r'.*\bof\b.*')
-#     rels = extract_rels('LOCATION', 'GPE', nameEntity, corpus='ace', pattern=OF, window=10)
-#     rels2 = extract_rels('LOCATION', 'GPE', nameEntity, corpus='ace', pattern=OF2, window=10)
-#     if rels or rels2:
-#         print(nameEntity)
-#
-#     print(rels)
-#     print(rels2)
+nlp = spacy.load("en_core_web_md")
+doc = nlp(fl)
 
-# nameEntity.draw()
-# IN = re.compile(r'.*\bin\b(?!\b.+ing)')
-# res = nltk.sem.extract_rels('ORG', 'LOC', pos_tag[1],corpus='ieer', pattern = IN)
-# print(res)
-# print(tokens)
-# pos_tag = [nltk.pos_tag(token) for token in tokens]
-# for each in pos_tag:
-#
-    # break
+relations = extract_currency_relations(doc)
+print(relations)
+for r1, r2 ,r3 in relations:
+    print("\t{}\t{}\t{}".format(r1.text, r2.text,r3.text))
+# labels = [x.label_ for x in doc.ents]
+# print(Counter(labels))
+html = displacy.render(doc,style='dep',page=True)
+outputfile = open('dep.html',"w",encoding='utf-8')
+outputfile.write(html)
 
-
-# import nltk
-# nltk.download('ieer')
-# from nltk.corpus import ieer
-#
-# # file = open("WikipediaArticles/Amazon_com.txt")
-# docs = ieer.parsed_docs('NYT_19980315')
-# tree = docs[1].text
-# print(tree)
+html2 = displacy.render(doc,style='ent',page=True)
+outputfile2 = open('ent.html',"w",encoding='utf-8')
+outputfile2.write(html2)
