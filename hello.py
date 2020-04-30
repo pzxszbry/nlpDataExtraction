7182c20b9dd0b313505ecd08e456c6ba4e227405
YI ZHENG <yizheng@YIs-MacBook-Pro.local>
2020-04-30 11:23:09 -0500

    using matcher for try

--- hello.py
+++ hello.py
@@ -3,93 +3,133 @@ import nltk
 from collections import Counter
 from nltk.stem import WordNetLemmatizer
 from spacy import displacy
-from nltk.chunk import conlltags2tree, tree2conlltags
-
-import re
-from nltk.sem.relextract import extract_rels, rtuple
+from spacy.matcher import Matcher
 lemmatizer = WordNetLemmatizer()
+class Solution:
 
-def filter_spans(spans):
-    # Filter a sequence of spans so they don't contain overlaps
-    # For spaCy 2.1.4+: this function is available as spacy.util.filter_spans()
-    get_sort_key = lambda span: (span.end - span.start, -span.start)
-    sorted_spans = sorted(spans, key=get_sort_key, reverse=True)
-    result = []
-    seen_tokens = set()
-    for span in sorted_spans:
-        # Check for end - 1 here because boundaries are inclusive
-        if span.start not in seen_tokens and span.end - 1 not in seen_tokens:
-            result.append(span)
-        seen_tokens.update(range(span.start, span.end))
-    result = sorted(result, key=lambda span: span.start)
-    return result
-#     return relations
-def extract_currency_relations(doc):
-    spans = list(doc.ents) + list(doc.noun_chunks)
-    spans = filter_spans(spans)
-    with doc.retokenize() as retokenizer:
-        for span in spans:
-            retokenizer.merge(span)
-    wordSet = set()
-    relations = []
-    for word in filter(lambda w: w.pos_=="VERB" or w.pos_=="NOUN", doc):
-        # print(word)
-        similarWords = ["acquire","buy","purchase"]
-        for sigleSimilarWords in similarWords:
-            # print(word)
-            if nlp(word.lemma_).similarity(nlp(sigleSimilarWords))>0.7:
-                wordSet.add(word)
-    print(wordSet)
-    for word in wordSet:
-        # print(word.head)
-        childs = word.children
-        # print(list(childs))
-        print(childs.__class__)
-        dobj = []
-        nsubj = []
-        for possible in word.children:
-            print(possible)
-            if possible.dep_ == "nsubj" and (possible.ent_type_ =="ORG" or possible.pos_=="PRON"):
-                nsubj.append(possible)
-            elif possible.dep_=='dobj' and (possible.ent_type_ =="ORG" or possible.pos_=="PRON"):
-                dobj.append(possible)
-        for i in dobj:
-            for j in nsubj:
-                relations.append((j,word,i))
-    return relations
-def lemmatize(tokens):
-    lemmaArr = []
-    for token in tokens:
-        temp = []
-        for word in token:
-            temp.append(lemmatizer.lemmatize(word))
-        lemmaArr.append(temp)
-    return lemmaArr;
+    nlp = spacy.load("en_core_web_md")
+    matched_sents = []
 
-file = open("WikipediaArticles/Amazon_com.txt")
-# file = open("WikipediaArticles/IBM.txt")
-# file = open("WikipediaArticles/AppleInc.txt")
-# file = open("test.txt")
+    def filter_spans(self,spans):
+        # Filter a sequence of spans so they don't contain overlaps
+        # For spaCy 2.1.4+: this function is available as spacy.util.filter_spans()
+        get_sort_key = lambda span: (span.end - span.start, -span.start)
+        sorted_spans = sorted(spans, key=get_sort_key, reverse=True)
+        result = []
+        seen_tokens = set()
+        for span in sorted_spans:
+            # Check for end - 1 here because boundaries are inclusive
+            if span.start not in seen_tokens and span.end - 1 not in seen_tokens:
+                result.append(span)
+            seen_tokens.update(range(span.start, span.end))
+        result = sorted(result, key=lambda span: span.start)
+        return result
+    #     return relations
 
-fl = file.read()
-sentences = nltk.sent_tokenize(fl) # Split the document into sentences
-tokens = [nltk.word_tokenize(sentence) for sentence in sentences] # Tokenize the sentences into words
-lemmaArr = lemmatize(tokens)  # Lemmatize the words to extract lemmas as features
-pos_tag = [nltk.pos_tag(token) for token in lemmaArr] # Part-of-speech (POS) tag the words to extract POS tag features
+    def collect_sents(self,matcher, doc, i, matches):
+        match_id, start, end = matches[i]
+        span = doc[start:end]  # Matched span
+        sent = span.sent  # Sentence containing matched span
+        # Append mock entity for match in displaCy style to matched_sents
+        # get the match span by ofsetting the start and end of the span with the
+        # start and end of the sentence in the doc
+        match_ents = [{
+            "start": span.start_char - sent.start_char,
+            "end": span.end_char - sent.start_char,
+            "label": "MATCH",
+        }]
+        self.matched_sents.append({"text": sent.text, "ents": match_ents})
 
-nlp = spacy.load("en_core_web_md")
-doc = nlp(fl)
+    def extract_currency_relations(self,doc):
+        spans = list(doc.ents) + list(doc.noun_chunks)
+        spans = self.filter_spans(spans)
+        with doc.retokenize() as retokenizer:
+            for span in spans:
+                retokenizer.merge(span)
+        matcher = Matcher(self.nlp.vocab,validate=True)
+        #
+        # for tok in doc[:5]:
+        #     print(tok.text, "-->", tok.dep_, "-->", tok.pos_,"-->",tok.ent_type_)
+        pattern1 = [{'ENT_TYPE': 'ORG'},
+                   {'LEMMA': {"IN":["buy","acquire","sell"]}},
+                   {'ENT_TYPE': 'ORG'}]
 
-relations = extract_currency_relations(doc)
-print(relations)
-for r1, r2 ,r3 in relations:
-    print("\t{}\t{}\t{}".format(r1.text, r2.text,r3.text))
-# labels = [x.label_ for x in doc.ents]
-# print(Counter(labels))
-html = displacy.render(doc,style='dep',page=True)
-outputfile = open('dep.html',"w",encoding='utf-8')
-outputfile.write(html)
+        # pattern2 = [{'ENT_TYPE': 'ORG'},
+        #            {'LEMMA': {"IN":["buy","acquire","sell"]}},
+        #            {'ENT_TYPE': 'ORG'}]
 
-html2 = displacy.render(doc,style='ent',page=True)
-outputfile2 = open('ent.html',"w",encoding='utf-8')
-outputfile2.write(html2)
+        matcher.add("CompanyOwn",self.collect_sents,pattern1)
+
+        matches = matcher(doc)
+        for match_id, start, end in matches:
+            string_id = self.nlp.vocab.strings[match_id]  # Get string representation
+            span = doc[start:end]  # The matched span
+            print(match_id, string_id, start, end, span.text)
+
+        # wordSet = set()
+        # relations = []
+        # for word in filter(lambda w: w.pos_=="VERB" or w.pos_=="NOUN", doc):
+        #     # print(word)
+        #     similarWords = ["acquire","buy","purchase"]
+        #     for sigleSimilarWords in similarWords:
+        #         # print(word)
+        #         if nlp(word.lemma_).similarity(nlp(sigleSimilarWords))>0.7:
+        #             wordSet.add(word)
+        # print(wordSet)
+        # for word in wordSet:
+        #     # print(word.head)
+        #     childs = word.children
+        #     # print(list(childs))
+        #     print(childs.__class__)
+        #     dobj = []
+        #     nsubj = []
+        #     for possible in word.children:
+        #         print(possible)
+        #         if possible.dep_ == "nsubj" and (possible.ent_type_ =="ORG" or possible.pos_=="PRON"):
+        #             nsubj.append(possible)
+        #         elif possible.dep_=='dobj' and (possible.ent_type_ =="ORG" or possible.pos_=="PRON"):
+        #             dobj.append(possible)
+        #     for i in dobj:
+        #         for j in nsubj:
+        #             relations.append((j,word,i))
+        # return relations
+    def lemmatize(self,tokens):
+        lemmaArr = []
+        for token in tokens:
+            temp = []
+            for word in token:
+                temp.append(lemmatizer.lemmatize(word))
+            lemmaArr.append(temp)
+        return lemmaArr;
+
+    def main(self):
+        file = open("WikipediaArticles/Amazon_com.txt")
+        # file = open("WikipediaArticles/IBM.txt")
+        # file = open("WikipediaArticles/AppleInc.txt")
+        # file = open("test.txt")
+
+        fl = file.read()
+        sentences = nltk.sent_tokenize(fl) # Split the document into sentences
+        tokens = [nltk.word_tokenize(sentence) for sentence in sentences] # Tokenize the sentences into words
+        lemmaArr = self.lemmatize(tokens)  # Lemmatize the words to extract lemmas as features
+        pos_tag = [nltk.pos_tag(token) for token in lemmaArr] # Part-of-speech (POS) tag the words to extract POS tag features
+
+        doc = self.nlp(fl)
+        # doc = nlp("Amazon acquired Whole Foods Market for US$13.4 billion.")
+        relations = self.extract_currency_relations(doc)
+
+        # print(relations)
+        #
+        # # labels = [x.label_ for x in doc.ents]
+        # # print(Counter(labels))
+        html = displacy.render(doc,style='dep',page=True)
+        outputfile = open('dep.html',"w",encoding='utf-8')
+        # outputfile.write(html)
+        #
+        # html2 = displacy.render(doc,style='ent',page=True)
+        html2= displacy.render(self.matched_sents, style="ent", manual=True)
+        outputfile2 = open('ent.html',"w",encoding='utf-8')
+        outputfile2.write(html2)
+if __name__=='__main__':
+    s = Solution()
+    s.main();
